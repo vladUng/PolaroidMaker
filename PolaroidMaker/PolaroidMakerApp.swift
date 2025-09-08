459cb473ed8ad0d976b64fbfa25c616ec794e23f
Vlad Ungureanu <vlad2403@gmail.com>
2025-09-08 22:32:19 +0300

    Initial Commit

---
+++ PolaroidMaker/PolaroidMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolaroidMakerApp.swift
+//  PolaroidMaker
+//
+//  Created by Vlad Ungureanu on 08/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct PolaroidMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PolaroidMakerTests/PolaroidMakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PolaroidMakerTests.swift
+//  PolaroidMakerTests
+//
+//  Created by Vlad Ungureanu on 08/09/2025.
+//
+
+import Testing
+@testable import PolaroidMaker
+
+struct PolaroidMakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
